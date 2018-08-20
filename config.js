f19fa40216b7bfdf63a99a9f72e448df73fd639a
Michael Gladstein <mtg@Michaels-MacBook-Pro-2.local>
2018-08-20 12:34:00 -0400

    changed password

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ module.exports = {
     dbUrl: "mongodb://mtg:mtg321@ds215172.mlab.com:15172/dialogflow-demo",
     tenancyName: "Roboyo_JBEARD",
     usernameOrEmailAddress: "Jbeard",
-    password: "P@ssword1",
+    password: "P@ssword2",
     auth: "https://platform.uipath.com/api/account/authenticate",
     queueUrl: "https://platform.uipath.com/odata/Queues/UiPathODataSvc.AddQueueItem"
 }
