099ca10347c0dca54afc05ace9c74ef767963e21
Fester527 <35502356+Fester527@users.noreply.github.com>
2019-07-18 09:07:41 -0400

    Update config.js
    Removed credentials

--- config.js
+++ config.js
@@ -1,13 +1,13 @@
 'use strict';
 
 module.exports = {
-    dbUrl: "mongodb://mtg:mtg321@ds215172.mlab.com:15172/dialogflow-demo",
+    dbUrl: "",
     // tenancyName: "Training",
     // usernameOrEmailAddress: "Jon",
     // password: "123FromNYtoGermany",
-    tenancyName: "Roboyo_JBEARD",
-    usernameOrEmailAddress: "jbeard",
-    password: "P@ssword2",
+    tenancyName: "",
+    usernameOrEmailAddress: "",
+    password: "",
     // auth: "https://orchestratorrby.roboyoapps.com/api/Account/authenticate",
     // queueUrl: "https://orchestratorrby.roboyoapps.com/odata/Queues/UiPathODataSvc.AddQueueItem",
     auth: "https://platform.uipath.com/api/Account",
